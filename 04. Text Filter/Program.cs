eb19333f5c598e88022edace39494e7534db9299
Admin <Admin@DESKTOP-QFVKQ9F>
2022-06-22 21:43:22 +0300

    Add project files.

---
+++ 04. Text Filter/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace _04._Text_Filter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string strings = Console.ReadLine();
+            string[] stringToReplace = strings.Split(",");
+            string text = Console.ReadLine();
+            foreach (var word in stringToReplace)
+            {
+                string asteriks = string.Empty;
+                for (int i = 0; i < word.Length; i++)
+                {
+                    asteriks +='*';
+                }
+                text = text.Replace(word, asteriks);
+            }
+            Console.WriteLine(text);
+        }
+    }
+}
---
+++ 5. Digits, Letters and Other/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Linq;
+using System.Text;
+
+namespace _5._Digits__Letters_and_Other
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string text = Console.ReadLine();
+            var letters = new StringBuilder();
+            var nums = new StringBuilder();
+            var other = new StringBuilder();
+            for (int i = 0; i < text.Length; i++)
+            {
+                var chartext = text[i];
+                if (char.IsDigit(chartext))
+                {
+                    nums.Append(chartext);
+                }
+                else if (char.IsLetter(chartext))
+                {
+                    letters.Append(chartext);
+                }
+                else
+                {
+                    other.Append(chartext);
+                }
+            }
+            Console.WriteLine($"{nums}\n{letters}\n{other}");
+        }
+    }
+}
